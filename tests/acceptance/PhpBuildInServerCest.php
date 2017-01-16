7fd76be6ebc7608b36200f8fb44fa24947b29b0a
Mario Näther <m.naether@queo-group.com>
2017-01-16 19:51:05 +0100

    - fixed  "phpbuildinserver"  test

--- tests/acceptance/PhpBuildInServerCest.php
+++ tests/acceptance/PhpBuildInServerCest.php
@@ -12,9 +12,10 @@ class PhpBuildInServerCest
     }
 
     // tests
-    public function php_build_server_should_be_run(AcceptanceTester $I)
+    public function php_buildin_server_should_be_run(AcceptanceTester $I)
     {
-        $I->amOnUrl('http://localhost:8000/index.php');
+        $I->amOnUrl('http://localhost:8000');
+        $I->amOnPage('/index.php');
         $I->see('Foo Bar Server is Running');
     }
 }
