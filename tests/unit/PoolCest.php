2291784c60b0fc6a6028ac3f87cb17b7bae91522
Mario Näther <mario-naether@gmx.de>
2017-01-24 21:15:07 +0100

    - added module "codeception-dataprovider-module" for issue #4

--- tests/unit/PoolCest.php
+++ tests/unit/PoolCest.php
@@ -17,4 +17,13 @@ class PoolCest
         $I->seeDateIsToday(date('d.m.Y'));
         $I->canSeeDateInPast('24.12.2016');
     }
+
+    // tests
+    public function dataProviderShouldWork(UnitTester $I) {
+        $I->assertEquals('123', $I->getValue('users.admins.0.id'));
+        $I->iterateOver('users.admins', function($user, $index) use ($I) {
+            $I->assertTrue(is_numeric($user['id']));
+        });
+    }
+
 }
